111e9f4f8abf2aef08e64a05eb34a546c1bf9236
long0339637721 <127864774+long0339637721@users.noreply.github.com>
2023-03-16 17:01:34 +0700

    done

--- data/E-C-M.sql
+++ data/E-C-M.sql
@@ -1,1 +1,88 @@
--- Tạo bảng
+create database ECM;
+use ECM;
+
+DROP TABLE IF EXISTS User;
+CREATE TABLE User(
+	ID                  CHAR(6) primary key,
+    SSN                 VARCHAR(12) NOT NULL unique,
+    Email               VARCHAR(50),
+    Phone               VARCHAR(50),
+    FullName            VARCHAR(50),
+    Sex					char(1) check(Sex in ('M', 'F')),
+    Bdate				DATE,
+    Address				varchar(50),
+    Username            VARCHAR(50) NOT NULL unique,
+    Pass                VARCHAR(50) NOT NULL
+);
+DROP TABLE IF EXISTS Admin;
+CREATE TABLE Admin(
+	AID                  CHAR(6) primary key,
+    FOREIGN KEY (AID) REFERENCES User (ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+DROP TABLE IF EXISTS Manager;
+CREATE TABLE Manager(
+	MID                  CHAR(6) primary key,
+    FOREIGN KEY (MID) REFERENCES User (ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+DROP TABLE IF EXISTS Teacher;
+CREATE TABLE Teacher(
+	TID                  CHAR(6) primary key,
+    FOREIGN KEY (TID) REFERENCES User (ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+create table Student(
+	ID                  CHAR(6) primary key,
+    Email               VARCHAR(50),
+    Phone               VARCHAR(50),
+    FullName            VARCHAR(50),
+    Sex					char(1) check(Sex in ('M', 'F')),
+    Bdate				DATE,
+    Address				varchar(50)
+);
+create table Class(
+	CID                 CHAR(6) primary key,
+    So_buoi				INT check(So_buoi > 0),
+    NumStudent			INT check(NumStudent > 0),
+    DateS				Date,
+    DateE				Date,
+    Des					VARCHAR(300),
+    CType				char(2) check(CType in ('1A', '2A', '3A', '4A')),
+    TID			CHAR(6) not null,
+    FOREIGN KEY (TID) REFERENCES Teacher (TID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+create table Room(
+	RID                 CHAR(6) primary key,
+    Name				varchar(10)
+);
+create table Buoi_hoc(
+	ID                 	CHAR(6) primary key,
+    CID					CHAR(6) not null,
+    RID					CHAR(6) not null,
+    TID					CHAR(6),
+    MID					CHAR(6),
+    Ngay				Date,
+    tiet_bat_dau		int check(tiet_bat_dau > 1 & tiet_bat_dau < 12),
+    so_tiet				int check(so_tiet > 0 & so_tiet < 4),
+    note				varchar(300),
+    ID_bu				CHAR(6),
+    FOREIGN KEY (CID) REFERENCES Class (CID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (RID) REFERENCES Room (RID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (TID) REFERENCES Teacher (TID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (MID) REFERENCES Manager (MID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (ID_bu) REFERENCES Buoi_hoc (ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+create table thuoc(
+	CID					CHAR(6),
+    SID					CHAR(6),
+    CONSTRAINT thuoc_pk PRIMARY KEY (CID, SID),
+    FOREIGN KEY (CID) REFERENCES Class (CID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (SID) REFERENCES Student (ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+create table hoc(
+	BID					CHAR(6),
+    SID					CHAR(6),
+    CONSTRAINT thuoc_pk PRIMARY KEY (BID, SID),
+    note				varchar(50),
+    status				char(1) check(status in ('V', 'T', 'C')), -- vang, tre, co mat
+    FOREIGN KEY (BID) REFERENCES Buoi_hoc (ID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (SID) REFERENCES Student (ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
