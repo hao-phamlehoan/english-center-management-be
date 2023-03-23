427781e81e01bb8063780c76014e7048fd0e3dfe
hao-phamlehoan <hao.phamlehoan@hcmut.edu.vn>
2023-03-24 06:34:44 +0700

    api room

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,12 +1,13 @@
 const userRoute = require('./user.route')
 const studentRoute = require('./student.route')
 const classRoute = require('./class.route')
-
+const roomRoute = require('./room.route')
 
 const route = (app) => {
   app.use('/user', userRoute)
   app.use('/student', studentRoute)
   app.use('/class', classRoute)
+  app.use('/room', roomRoute)
 }
 
 module.exports = route
---
+++ src/routes/lesson.route.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const roomController = require('../controllers/room.controller')
+
+router.get('/all', roomController.getAll)
+router.get('/:id', roomController.get)
+router.get('/student/:id', roomController.getClassInClass)
+router.delete('/:id', roomController.delete)
+router.post('/create', roomController.create)
+router.patch('/:id', roomController.update)
+
+module.exports = router
